00b4166eac064d5b3cca218b979b52e9824be2b5
dellson-arch <nayanmahato908@gmail.com>
2026-05-07 00:50:59 +0530

    Card docs

---
+++ apps/docs/config/docs.js
@@ -0,0 +1,32 @@
+export const docsConfig = {
+  mainNav: [
+    {
+      title: "Docs",
+      href: "/docs",
+    },
+  ],
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [
+        {
+          title: "Introduction",
+          href: "/docs",
+        },
+      ],
+    },
+    {
+      title: "Components",
+      items: [
+        {
+          title: "Button",
+          href: "/docs/button",
+        },
+        {
+          title: "Card",
+          href: "/docs/card",
+        },
+      ],
+    },
+  ],
+};

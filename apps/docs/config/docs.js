b0feb748d0636098cef81b05bb9bbc5a845dd2e3
dellson-arch <nayanmahato908@gmail.com>
2026-05-07 02:07:53 +0530

    docs.js and index.js conflict resolved

--- apps/docs/config/docs.js
+++ apps/docs/config/docs.js
@@ -30,32 +30,4 @@ export const docsConfig = {
       ],
     },
   ],
-=======
-    mainNav: [
-        {
-            title: "Docs",
-            href: "/docs",
-        },
-    ],
-    sidebarNav: [
-        {
-            title: "Getting Started",
-            items: [
-                {
-                    title: "Introduction",
-                    href: "/docs",
-                },
-            ],
-        },
-        {
-            title: "Components",
-            items: [
-                {
-                    title: "Button",
-                    href: "/docs/button",
-                },
-            ],
-        },
-    ],
-
-};
+}

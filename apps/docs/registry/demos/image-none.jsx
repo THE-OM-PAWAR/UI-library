7c8f2df23c9fef7f082aa16dd56be00774e9689c
Harshit Jain <harshitostwal1234@gmail.com>
2026-05-13 13:37:49 +0530

    refactor(demos/image): update imports to @shery-ui/*

--- apps/docs/registry/demos/image-none.jsx
+++ apps/docs/registry/demos/image-none.jsx
@@ -1,4 +1,4 @@
-import { Image } from "@repo/components";
+import { Image } from "@shery-ui/components";
 
 const src =
     "https://images.unsplash.com/photo-1774711268987-a56e0de1d79d?q=80&w=800";

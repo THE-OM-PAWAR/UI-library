0436831a49accef6f0aa1405e5076aff699d6f26
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-21 15:15:21 +0530

    Added micr interactions in the bg using thr white snow flares

--- apps/docs/app/page.jsx
+++ apps/docs/app/page.jsx
@@ -2,6 +2,7 @@ import ContributionForm from '@/components/home/ContributionForm'
 import ComponentShowcase from '@/components/home/ComponentShowcase'
 import EndBrand from '@/components/home/EndBrand'
 import FAQ from '@/components/home/FAQ'
+import FloatingParticles from '@/components/home/FloatingParticles'
 import Footer from '@/components/home/Footer'
 import Hero from '@/components/home/Hero'
 import PerformanceDX from '@/components/home/PerformanceDX'
@@ -10,6 +11,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
+      <FloatingParticles />
       <Hero />
       <ComponentShowcase />
       <PerformanceDX />

2f3101444de07a65d93f94fc293cdc67b1e9e339
Nihal Uikey <nihaluikey6488@gmail.com>
2026-06-03 01:03:24 +0530

    feat: multi-direction sheet added

--- apps/docs/registry/demos/sheet-demo.jsx
+++ apps/docs/registry/demos/sheet-demo.jsx
@@ -1,17 +1,47 @@
-import { Button, Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@repo/components";
+import { Button } from "@repo/components"
+import { Input } from "@repo/components"
+import { Label } from "@repo/components"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@repo/components"
 
 export default function SheetDemo() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">Open sheet</Button>
+        <Button variant="outline">Open</Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>Make quick updates without leaving the page.</SheetDescription>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
         </SheetHeader>
+        <div className="flex flex-col gap-5  flex-1 auto-rows-min px-4">
+          <div className="flex flex-col gap-4">
+            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="flex flex-col gap-4">
+            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Input id="sheet-demo-username" defaultValue="@peduarte" />
+          </div>
+        </div>
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
       </SheetContent>
     </Sheet>
-  );
+  )
 }
